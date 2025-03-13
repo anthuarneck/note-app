bec5470858a14efafdec3d226828a1e6d92cceb8
Anthony Huarneck <anthuarneck@me.com>
2025-03-12 20:23:09 -0400

    Adds textbox and markdown functionality to Note.jsx, adds Note component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Markdown from 'react-markdown'
 import './App.css'
+import Note from './Components/Note'
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <h1>Notes</h1>
-
+      <Note />
     </>
   )
 }
