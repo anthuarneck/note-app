925220c2613a0d1411c35e6a15c0dd70f895559c
Anthony Huarneck <anthuarneck@me.com>
2025-03-13 20:21:09 -0400

    Installs Tailwind, implements tailwind columns, reorganizes app.jsx, temporarily removes body css in index.css file page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import Markdown from 'react-markdown'
-import './App.css'
-import Note from './Components/Note'
-
+import { useState } from "react";
+import Markdown from "react-markdown";
+import "./App.css";
+import Note from "./Components/Note";
+import Notes from "./Components/Notes";
 
 function App() {
-
   return (
-    <>
-      <h1>Notes</h1>
-      <Note />
-    </>
-  )
+    <div class="columns-2">
+      <div class="aspect-3/2">
+        <Notes />
+      </div>
+      <div class="aspect-3/2">
+        <h1>Current Note</h1>
+        <Note />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useState } from 'react'
-import Markdown from 'react-markdown'
+import React from "react";
+import { useState } from "react";
+import Markdown from "react-markdown";
 
 export default function Note() {
-    const [text, setText] = useState("")
+  const [text, setText] = useState("");
 
-    const handleChange = (event) => {
-        setText(event.target.value)
-    }
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
 
   return (
     <div>
-      <input type="text" value ={text} onChange={handleChange} />
+      <input type="text" value={text} onChange={handleChange} />
       <Markdown>{text}</Markdown>
     </div>
-  )
+  );
 }
